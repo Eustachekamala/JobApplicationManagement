7c1c19cd692496050044da9bc62fce598b68f098
eustachekamala <eustachejared25@gmail.com>
2025-09-23 23:28:58 +0300

    added the swagger api

--- src/main/java/org/eustache/management_systeme/JobApplicationManagement.java
+++ src/main/java/org/eustache/management_systeme/JobApplicationManagement.java
@@ -1,8 +1,18 @@
 package org.eustache.management_systeme;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Job Application Management API",
+                version = "1.0",
+                description = "API for managing job applications, including CRUD operations and status tracking."
+        )
+)
 @SpringBootApplication
 public class JobApplicationManagement {
 
